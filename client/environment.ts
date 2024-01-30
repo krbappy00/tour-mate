6d59f4d83a3217d3710404fd7506f743707ac3c4
Khalisur Bappy <118562588+krbappy00@users.noreply.github.com>
2024-01-31 01:47:48 +0600

    Delete client/environment.ts

--- client/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  stripe: {
-    publicKey: 'pk_test_51O5KiDCnPh0b6YPIkYdVswvvaDafbAbnpjkEmPNRU9szI9gz3XFUgGu47OyA4wYHmMknxrW4TqZKr3FakZFRJEO600KtfnP3a5',
-  }
-};
